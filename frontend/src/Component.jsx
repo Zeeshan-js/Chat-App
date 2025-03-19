0071c75fa20f6d3fea35386dd8d4278aa0d52ca5
Zeeshan Malik <kamaruu95@gmail.com>
2025-03-19 16:35:01 +0530

    rearrangements

--- frontend/src/Component.jsx
+++ frontend/src/Component.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Menu, Search } from "lucide-react";
 import axios from "axios";
-import Chat from "./Chat.jsx"
-
+import List from "./List.jsx"
+import Messages from "./Messages.jsx"
 
 function Component() {
 
@@ -12,8 +12,9 @@ function Component() {
 
   return (
     <div className="h-screen flex items-center justify-center bg-[#222831]">
-      <div className="border h-9/12 w-8/12 rounded-3xl bg-[#393E46]">
-      <Chat />
+      <div className="flex border h-9/12 w-8/12 rounded-3xl bg-[#393E46]">
+      <List />
+      <Messages />
       </div>
     </div>
   );
---
+++ frontend/src/Messages.jsx
@@ -0,0 +1,13 @@
+
+
+function Messages() {
+
+
+    return (
+        <div>
+            This is messages window
+        </div>
+    )
+}
+
+export default Messages
